14b5f8a48f0413ef8442b69abf327917a9db82bf
Tagir Magomedov <tmagomedov@twitter.com>
2015-07-17 02:36:53 -0700

    Add debug info

--- core/src/main/scala/info/fotm/clustering/FeatureEvaluatorApp.scala
+++ core/src/main/scala/info/fotm/clustering/FeatureEvaluatorApp.scala
@@ -19,8 +19,9 @@ object FeatureEvaluatorApp extends App {
   val end = start + 2 * settings.turnsPerWeek
   val data: Stream[DataPoint] = dataGen.updatesStream().slice(start, end)
 
-  val (prevLadder, lastladder, _) = data.last
-  lastladder.rows.toList.sortBy(-_._2.stats.rating).map(_._2.stats).foreach(println)
+  // uncomment following line for viewing ladder state
+  //val (prevLadder, lastladder, _) = data.last
+  //lastladder.rows.toList.sortBy(-_._2.stats.rating).map(_._2.stats).foreach(println)
 
   def estimate(fs: Seq[Feature[CharacterStatsUpdate]]): Double = {
     val clusterer = RealClusterer.wrap(new ClosestClusterer())
@@ -30,6 +31,7 @@ object FeatureEvaluatorApp extends App {
     1 - evaluator.evaluate(clusterer, data)
   }
 
+  // uncomment following lines to continue search from predefined weights
 //  val startingWeights = MathVector(2.179498600996277,1.593301777294845,1.817542615061698,0.27151402114766976,0.7440438353774109,0.7438512713247858,-0.041006394687579384,0.763906482541476,0.01250563169272434,0.798455058753051,0.005920247667347223,0.8278914956535278,0.2491892886456094,0.8060805544258177,0.0797461392031601,1.571142710682229,0.247217196929698,0.7530892028608863)
 //  val weightedFeatures = Feature.reweigh(ClusteringEvaluatorApp.features.zip(startingWeights.coords))
 //  val weights = ML.findWeights(weightedFeatures, estimate)
