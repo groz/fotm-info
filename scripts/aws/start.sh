0c72b10bdf545aae92284dd05c8966614b1b009c
Tagir Magomedov <tmagomedov@twitter.com>
2015-08-02 01:35:45 -0700

    Add validation

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# stop apache if it's running for some reason
-# probably because it was a demo CodeDeploy fleet
-# created for WordPress app
-apachectl stop
-
-# start portal & crawler
-nohup /fotm-app/portal-1.0-SNAPSHOT/bin/portal -Dhttp.port=80 &
-nohup java -jar /fotm-app/crawler-assembly-1.0-SNAPSHOT.jar &
+# start portal
+/fotm-app/portal-1.0-SNAPSHOT/bin/portal -Dhttp.port=80 > /dev/null 2> /dev/null < /dev/null &
--- scripts/aws/stop.sh
+++ scripts/aws/stop.sh
@@ -3,10 +3,6 @@
 # stop running portal app
 cat /fotm-app/portal-1.0-SNAPSHOT/RUNNING_PID | xargs kill -SIGTERM
 
-# stop running portal & crawler
-ps aux | grep "[p]ortal" | awk '{print $2}' | xargs kill -9
-ps aux | grep "[c]rawler" | awk '{print $2}' | xargs kill -9
-
 # delete RUNNING_PID file if it exists
 rm -f /fotm-app/portal-1.0-SNAPSHOT/RUNNING_PID
 
