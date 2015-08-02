527d58a98bb7e564edfe14e1e8edcf38a0323be6
Tagir Magomedov <tmagomedov@twitter.com>
2015-08-01 23:51:43 -0700

    Scripts adjusted. Portal & crawler at the same time

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd ~
+# stop apache if it's running for some reason
+# probably because it was a demo CodeDeploy fleet
+# created for WordPress app
+apachectl stop
 
-/fotm-app/portal-1.0-SNAPSHOT/bin/portal -Dhttp.port=80 &
-disown
-
-java -jar /fotm-app/crawler-assembly-1.0-SNAPSHOT.jar &
-disown
+# start portal & crawler
+nohup /fotm-app/portal-1.0-SNAPSHOT/bin/portal -Dhttp.port=80 &
+nohup java -jar /fotm-app/crawler-assembly-1.0-SNAPSHOT.jar &
