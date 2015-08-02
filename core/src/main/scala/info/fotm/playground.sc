16f77ae7b62a46ef5c5b671bcafb99e3a9b8fe8c
Hasan <hasan.akniyev@gmail.com>
2015-08-03 00:28:55 +0300

    clustering test

--- core/src/main/scala/info/fotm/playground.sc
+++ core/src/main/scala/info/fotm/playground.sc
@@ -1,8 +1,27 @@
+import info.fotm.util.MathVector
 import scala.collection.GenIterable
 import scala.collection.generic.CanBuildFrom
 import scala.collection.breakOut
 import scala.collection.immutable.IndexedSeq
-
+import info.fotm.clustering.implementations._
+  val x = new HTClusterer3()
+  val x11 = new MathVector(List(1.1, 1.0))
+  val x12 = new MathVector(List(1.0, 1.1))
+  val x13 = new MathVector(List(0.9, 1.1))
+  val x21 = new MathVector(List(-1.1, 1.0))
+  val x22 = new MathVector(List(-1.2, 0.9))
+  val x23 = new MathVector(List(-1.0, 1.1))
+  val x31 = new MathVector(List(1.1, -1.0))
+  val x32 = new MathVector(List(1.0, -1.1))
+  val x33 = new MathVector(List(1.0, -0.9))
+  val x41 = new MathVector(List(-1.1, -0.9))
+  val x42 = new MathVector(List(-1.0, -1.1))
+  val x43 = new MathVector(List(-0.9, -1.1))
+  val set = Seq(x11, x12, x13, x21, x22, x23, x31, x32, x33, x41, x42, x43)
+  val r1 = x.clusterize(set, 3).map(x => x.sortBy(y => y(0) * 1000 + y(1)))
+  var r2 = Set(Seq(x11, x12, x13), Seq(x21, x22, x23), Seq(x31, x32, x33), Seq(x41, x42, x43)).map(x => x.sortBy(y => y(0) * 1000 + y(1)))
+  r1 == r2
+/*
 def batch[From[A] <: Traversable[A], A, To[_] <: Traversable[_]]
     (from: From[A], batchSize: Int)
     (implicit
@@ -35,3 +54,4 @@ def batch[From[A] <: Traversable[A], A, To[_] <: Traversable[_]]
 val lst: List[Int] = (0 to 10).toList
 
 batch[List, Int, List](lst, 3)(breakOut, breakOut)
+*/
--- core/src/test/scala/HTClusterer3Spec.scala
+++ core/src/test/scala/HTClusterer3Spec.scala
@@ -8,7 +8,7 @@ import org.scalatest._
 class HTClusterer3Spec extends FlatSpec with Matchers with ClustererSpecBase {
   var htc3 = new HTClusterer3()
 
-  "Distance from (0,0) to [(1,1), (-1,1), (-1,-1), (1,-1)]" should "equals to sqrt(2)" in {
+  "HTC3.distTo" should "equals to sqrt(2)" in {
     val a = new MathVector(Seq(0, 0))
     val v1 = new MathVector(Seq(1, 1))
     val v2 = new MathVector(Seq(-1, 1))
@@ -19,4 +19,32 @@ class HTClusterer3Spec extends FlatSpec with Matchers with ClustererSpecBase {
 
     d should be(math.sqrt(2))
   }
+
+  it should "throw exception on empty set" in {
+    a [UnsupportedOperationException] should be thrownBy {
+      htc3.distTo(new MathVector(Seq(0,1)), Seq())
+    }
+  }
+
+  "HTC3.clusterize" should "works correctly" in {
+    val x = new HTClusterer3()
+    val x11 = new MathVector(List(1.1, 1.0))
+    val x12 = new MathVector(List(1.0, 1.1))
+    val x13 = new MathVector(List(0.9, 1.1))
+    val x21 = new MathVector(List(-1.1, 1.0))
+    val x22 = new MathVector(List(-1.2, 0.9))
+    val x23 = new MathVector(List(-1.0, 1.1))
+    val x31 = new MathVector(List(1.1, -1.0))
+    val x32 = new MathVector(List(1.0, -1.1))
+    val x33 = new MathVector(List(1.0, -0.9))
+    val x41 = new MathVector(List(-1.1, -0.9))
+    val x42 = new MathVector(List(-1.0, -1.1))
+    val x43 = new MathVector(List(-0.9, -1.1))
+    val set = Seq(x11, x12, x13, x21, x22, x23, x31, x32, x33, x41, x42, x43)
+    val r1 = x.clusterize(set, 3).map(x => x.sortBy(y => y(0) * 1000 + y(1)))
+    val r2 = Set(Seq(x11, x12, x13), Seq(x21, x22, x23), Seq(x31, x32, x33), Seq(x41, x42, x43)).map(x => x.sortBy(y => y(0) * 1000 + y(1)))
+    (r1 == r2) should be (true)
+  }
+
+
 }
