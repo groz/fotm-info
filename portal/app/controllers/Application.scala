36c00df09d73729705840f3b04574ab9e6e3dce0
Tagir Magomedov <tmagomedov@twitter.com>
2015-08-23 13:48:42 -0700

    Fix navbar offset for XS screen. Fix number of pages shown in navigation

--- portal/app/controllers/Application.scala
+++ portal/app/controllers/Application.scala
@@ -65,7 +65,7 @@ class Application @Inject()(system: ActorSystem) extends Controller {
       val nSetups = 20
 
       request.mapTo[Storage.QueryAllResponse].map { response =>
-        val totalPages = response.teams.size / perpage + 1
+        val totalPages = sca
         val teams = response.teams.slice(perpage * (page - 1), perpage * page) // 1-based indexing
         Ok(views.html.leaderboards(response.axis, response.setups.take(nSetups), teams, response.chars, minutes, perpage, page, totalPages))
       }
