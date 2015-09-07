46617fffda9e7daefb8559e810289022be1ae97d
Tagir Magomedov <tmagomedov@twitter.com>
2015-09-07 00:43:49 -0700

    Remove CN and TW from crawler/portal

--- core/src/main/scala/info/fotm/domain/Axis.scala
+++ core/src/main/scala/info/fotm/domain/Axis.scala
@@ -3,12 +3,15 @@ package info.fotm.domain
 import info.fotm.api.models._
 
 case object Axis {
+  val parsedRegions = List(US, Europe, China, Korea, Taiwan)
+  val parsedBrackets = List(Twos, Threes, Fives, Rbg)
+
   val allRegions = List(US, Europe, Korea) // China, Taiwan out of the loop to save resources
   val allBrackets = List(Twos, Threes, Fives, Rbg)
 
-  val regionMap: Map[String, Region] = allRegions.map(r => (r.slug, r)).toMap
+  val regionMap: Map[String, Region] = parsedRegions.map(r => (r.slug, r)).toMap
 
-  val bracketsMap: Map[String, Bracket] = allBrackets.map(b => (b.slug, b)).toMap
+  val bracketsMap: Map[String, Bracket] = parsedBrackets.map(b => (b.slug, b)).toMap
 
   val all = for {
     region <- allRegions
